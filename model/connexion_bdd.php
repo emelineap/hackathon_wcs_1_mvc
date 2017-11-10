cbc39dc329e2cd683f23dd401cd87e458e8b3db1
JennyViannay <jenny.vinnay75@gmail.com>
2017-11-10 01:31:49 +0100

    tiens bon Emeline

--- model/connexion_bdd.php
+++ model/connexion_bdd.php
@@ -1,13 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 // Création d'une connection à la base de base de donnée
-=======
-// Création d'une connection à la base de base de données
->>>>>>> 7851113e87e1a9541e9d8066807190393e72d144
 $bdd = mysqli_connect(SERVER, USER, PASS, DB);
 
-// Vérification que la connexion a fonctionné
+// Vérification que la connection à fonctionner
 if ($bdd == false){
 	echo "Connection error: " . mysqli_error($bdd);
 }
